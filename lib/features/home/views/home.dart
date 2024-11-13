1d4bf8ace953e1ace794ce4b501dcf99562eb81e
prasun kayastha <pkayastha31@gmail.com>
2024-11-13 15:13:38 +0545

    "Refactored HomeScreen UI: rearranged layout, added logout button, and modified search bar and list view"

--- lib/features/home/views/home.dart
+++ lib/features/home/views/home.dart
@@ -16,122 +16,142 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            height: 300,
             color: ColorConstant.primaryColor,
             child: Stack(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Row(
-                      children: [
-                        Text(
-                          'All Categories',
-                          style: FontConstant.buttonTextStyle,
-                        ),
-                        const Icon(
-                          Icons.arrow_drop_down,
-                          color: ColorConstant.secondaryColor,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.logout,
-                            color: ColorConstant.secondaryColor,
-                          ),
-                        ),
-                        Text(
-                          'Logout',
-                          style: FontConstant.buttonTextStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: ColorConstant.secondaryColor,
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: ColorConstant.accentColor,
-                          ),
-                          hintText: 'Search Product',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      // width: 350,
-                      color: ColorConstant.secondaryColor,
-                      child: Column(
-                        children: [
-                          ListView.builder(
-                            itemCount: 2,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return Card(
-                                child: ListTile(
-                                  leading: const CircleAvatar(
-                                    backgroundImage:
-                                        AssetImage('assets/logo/2logo.png'),
-                                    radius: 50,
-                                  ),
-                                  title: Text(
-                                    'User Admin',
-                                    style: FontConstant.cardTitle,
-                                  ),
-                                  trailing: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            'INR 100.00',
-                                            style: FontConstant.cardPrice,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            '111kg',
-                                            style: FontConstant.cardWeight,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 200),
+                  color: ColorConstant.primaryColor,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'All Categories',
+                                  style: FontConstant.buttonTextStyle,
+                                ),
+                                const Icon(
+                                  Icons.arrow_drop_down,
+                                  color: ColorConstant.secondaryColor,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.logout,
+                                    color: ColorConstant.secondaryColor,
                                   ),
                                 ),
-                              );
-                            },
-                          )
-                        ],
+                                Text(
+                                  'Logout',
+                                  style: FontConstant.buttonTextStyle,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: ColorConstant.secondaryColor,
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: ColorConstant.accentColor,
+                              ),
+                              hintText: 'Search Product',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 200,
+                  left: 16,
+                  right: 16,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: ColorConstant.secondaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            ListView.builder(
+                              itemCount: 6,
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return Card(
+                                  child: ListTile(
+                                    leading: const CircleAvatar(
+                                      backgroundImage:
+                                          AssetImage('assets/logo/2logo.png'),
+                                      radius: 30,
+                                    ),
+                                    title: Text(
+                                      'User Admin',
+                                      style: FontConstant.cardTitle,
+                                    ),
+                                    trailing: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'INR 100.00',
+                                              style: FontConstant.cardPrice,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              '111kg',
+                                              style: FontConstant.cardWeight,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
